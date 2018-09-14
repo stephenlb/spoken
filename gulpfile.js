4e0712f6eec3a742dc9e37e943a37fa0f34fde72
Stephen Blum <blum.stephen@gmail.com>
2018-09-14 16:24:42 -0700

    added Babel to gulp npm package.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp    = require('gulp');
 const wrapper = require('gulp-wrapper');
+const babel   = require('gulp-babel');
 
 gulp.task( 'spoken-module', e => {
     return gulp
     .src('src/spoken.js')
     .pipe(wrapper({ header : 'function spoken(){}\nexport default spoken;\n' }))
+    .pipe(babel())
     .pipe(gulp.dest('build'));
 } );
 
